3353960ac65d620c2322753ee19ebdfe46a10887
junhyeok4849 <junhyeok4849@gmail.com>
2025-05-20 17:53:28 +0900

    Add files via upload

---
+++ login.cy.js
@@ -0,0 +1,11 @@
+describe('로그인 기능 테스트', () => {
+  it('정상 로그인 시 홈화면 이동', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+
+    cy.get('input[name="username"]').type('Admin');
+    cy.get('input[name="password"]').type('admin123');
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/dashboard');
+  });
+});
